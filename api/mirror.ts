21a0a2d83ca8b8a43f706385cf089f5413cf7c76
Matias Rojas Z <matiasrz.dev@gmail.com>
2022-08-04 10:22:52 +0200

    File renaming, Serverless config & General improvements. - [Update] File renaming:   * Changing partials name to a more verbose way. - [Update] Serverless config:   * Updating setting keys & structure for more updated(v3).   * Removing deprecated keys. - [Update] Code:   * General improvements.

--- api/mirror.ts
+++ api/mirror.ts
@@ -1,5 +1,5 @@
 import handler from '@/lib/handler'
 
-const healthCheck = handler(async (): Promise<string> => "I'm alive!")
-
-export default healthCheck
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable-next-line @typescript-eslint/require-await */
+export const healthCheck = handler(async (): Promise<string> => "I'm alive!")
